09fac16513888b2aa61203e81081225e33ec1ff1
Zaklll <1015849735@qq.com>
2022-04-22 15:06:07 +0800

    feat: 新增分组控制功能

--- gee-web/main.go
+++ gee-web/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"gee"
@@ -9,39 +8,36 @@ import (
 
 func main() {
 	r := gee.New()
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
-	r.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-
-	r.GET("/hi/:user/do", func(c *gee.Context) {
-		log.Printf("/hi/:user/do")
-		c.String(http.StatusOK, "hello %s, you're at \n", c.Query("user"))
-	})
-
-	r.GET("/hi/*", func(c *gee.Context) {
-		log.Printf("/hi/*")
-		c.String(http.StatusOK, "hello %s, you're at \n", c.Query("user"))
-	})
-	r.GET("/hi/**", func(c *gee.Context) {
-		log.Printf("/hi/**")
-		c.String(http.StatusOK, "hello %s, you're at \n", c.Query("user"))
-	})
-
-	r.GET("/hi/v2/**", func(c *gee.Context) {
-		log.Printf("/hi/v2/**")
-		c.String(http.StatusOK, "hello %s, you're at \n", c.Query("user"))
-	})
-
-	r.POST("/login", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 		})
-	})
+
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := v1.Group("v2")
+
+	//v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
 
 	r.Run(":9999")
 }
