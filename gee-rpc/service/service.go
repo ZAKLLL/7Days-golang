be21b80540445108efd6f6b27fafee95e41f91a0
张家魁 <zhangjiakui@gci-china.com>
2022-08-23 17:42:44 +0800

    opt: 补充提交

--- gee-rpc/service/service.go
+++ gee-rpc/service/service.go
@@ -20,13 +20,16 @@ func (m *methodType) newArgv() reflect.Value {
 	var argv reflect.Value
 	//arg may be a pointer type， or a value Type
 	if m.ArgType.Kind() == reflect.Ptr {
+		//指针实例值
 		argv = reflect.New(m.ArgType.Elem())
 	} else {
+		//非指针实例值
 		argv = reflect.New(m.ArgType).Elem()
 	}
 	return argv
 }
 
+// 根据返回值类型，设置返回值
 func (m *methodType) newReplyv() reflect.Value {
 	//reply must be a pointer type
 	replyv := reflect.New(m.ReplyType.Elem())
@@ -37,4 +40,16 @@ func (m *methodType) newReplyv() reflect.Value {
 		replyv.Elem().Set(reflect.MakeSlice(m.ReplyType.Elem(), 0, 0))
 	}
 	return replyv
+}
+
+type service struct {
+	name   string                 //name 即映射的结构体的名称
+	typ    reflect.Type           //typ 是结构体的类型
+	rcvr   reflect.Value          //rcvr 即结构体的实例本身,保留 rcvr 是因为在调用时需要 rcvr 作为第 0 个参数
+	method map[string]*methodType //method 是 map 类型，存储映射的结构体的所有符合条件的方法。
+}
+
+func newService(rcvr interface{}) *service {
+	s := new(service)
+
 }
