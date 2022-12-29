986fffc50cf64f78d1ac96ad48e16fe03cd7c3c5
张家魁 <zhangjiakui@gci-china.com>
2022-12-29 17:31:37 +0800

    add:gee-orm

--- gee-orm/main.go
+++ gee-orm/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
 
+func main() {
+	db, _ := sql.Open("mysql", "root:gV0qUTyimTLhiqhiK@tcp(10.91.122.5:3307)/jyfx")
+	defer func() { _ = db.Close() }()
+
+	//row := db.QueryRow()
+
+	rows, err := db.Query("show tables")
+	if err != nil {
+		return
+	}
+	var name string
+	for rows.Next() {
+		if err := rows.Scan(&name); err == nil {
+			log.Println(name)
+		} else {
+			break
+		}
+
+	}
 }
